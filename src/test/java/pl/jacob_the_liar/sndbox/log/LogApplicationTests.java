aa82891fe4eb4f20b7969baec1e42aefa227603e
jakub <jacob_the_liar@tlen.pl>
2021-02-07 15:16:21 +0100

    initial commit

---
+++ src/test/java/pl/jacob_the_liar/sndbox/log/LogApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jacob_the_liar.sndbox.log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
